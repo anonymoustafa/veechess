d3aef06de954101a338b685a900b9e8f32be1542
anonymoustafa <anonymoustafa@outlook.com>
2022-09-17 19:42:55 +0430

    should i use struct to keep the state of the board? i am doing it now

--- veechess.v
+++ veechess.v
@@ -1,5 +1,41 @@
 module main
 
+// imports here
+import os
+import flag
+import rand
+// imports end here
+
 fn main() {
+	player_color := rand.intn(100) or {51}
+	if player_color <= 50 {println("you play as white")} else {println("you play as black")}
+	mut fp := flag.new_flag_parser(os.args)
+	println(fp.original_args[1]) 
 	println('Hello World!')
+
+mut state := Board{
+	a1: "b_bishop"
+}
+
+println(state)
+
+}
+
+
+fn pawn(){}
+fn bishop(){}
+fn knight(){}
+fn rook(){}
+fn queen(){}
+fn king(){}
+
+pub struct Board {
+	a1 string
+	a2 string
+	a3 string
+	a4 string
+	a5 string
+	a6 string
+	a7 string
+	a8 string
 }

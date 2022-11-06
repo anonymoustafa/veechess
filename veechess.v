9ec1e36473b26cb43103e458d9cfbfa8c9d9c58b
anonymoustafa <anonymoustafa@outlook.com>
2022-11-06 16:24:33 +0330

    some figuring it out is in progress

--- veechess.v
+++ veechess.v
@@ -7,35 +7,31 @@ import rand
 // imports end here
 
 fn main() {
-	player_color := rand.intn(100) or {51}
+	println('Welcome!')
+	player_color := rand.intn(100) or {5}
 	if player_color <= 50 {println("you play as white")} else {println("you play as black")}
 	mut fp := flag.new_flag_parser(os.args)
 	println(fp.original_args[1]) 
-	println('Hello World!')
 
-mut state := Board{
-	a1: "b_bishop"
-}
-
-println(state)
-
+	mut board_state := [][]string{len: 8, init: []string{len: 8}}
+board_state[0][1] = black_pawn()
+println(board_state) 
 }
 
 
-fn pawn(){}
-fn bishop(){}
-fn knight(){}
-fn rook(){}
-fn queen(){}
-fn king(){}
-
-pub struct Board {
-	a1 string
-	a2 string
-	a3 string
-	a4 string
-	a5 string
-	a6 string
-	a7 string
-	a8 string
+fn black_pawn() string{
+	return "black_pawn"
 }
+fn black_bishop_a(){}
+fn black_bishop_b(){}
+fn black_knight(){}
+fn black_rook(){}
+fn black_queen(){}
+fn black_king(){}
+fn white_pawn(){}
+fn white_bishop_a(){}
+fn white_bishop_b(){}
+fn white_knight(){}
+fn white_rook(){}
+fn white_queen(){}
+fn white_king(){}
